1aad444b85e9b082564430e30a4db195bfa28536
ZeeshanAslam1984 <zesh1984@gmail.com>
2025-10-08 21:14:36 +0500

    remove aws_ami variable; using data source to fetch latest Amazon Linux 2023 AMI

--- variables.tf
+++ variables.tf
@@ -1,14 +1,10 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "ap-southeast-2"
+  default     = "ap-southeast-2"
 }
 
 variable "aws_type" {
-  description = "AWS ec2 type"
-  default = "t2.micro"
+  description = "AWS EC2 instance type"
+  default     = "t2.micro"
 }
 
-variable "aws_ami" {
-  description = "AWS AMI"
-  default = "ami-03c4f11b50838ab5d"
-}
